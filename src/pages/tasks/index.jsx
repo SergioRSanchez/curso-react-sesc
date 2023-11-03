0beb17ef9813c5a0680b3691da9af246e51d6688
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-11-03 16:29:24 -0300

    feat: create home and routes to projects and tasks

---
+++ src/pages/tasks/index.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import '../../App.css'
+
+// import dataTask from './data/data-tasks.json'
+import dataTask from './../../data/data-tasks.json'
+
+// import { TaskForm, TasksList, Footer } from './components/index.js'
+import { TaskForm, TasksList, Footer } from '../../components'
+
+function App() {
+
+  const [tasks, setTasks] = useState(dataTask)
+
+  return (
+    <div className="app">
+      <TaskForm
+        tasks={tasks}
+        setTasks={setTasks}
+      />
+      <TasksList
+        tasks={tasks}
+        setTasks={setTasks}
+      />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
